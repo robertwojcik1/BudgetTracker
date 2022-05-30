5808ebe44e0657c89543563544f4b2f5e930cbb6
Robert Wójcik <robert.wojcik.programista@gmail.com>
2022-05-30 16:50:29 +0200

    added UserManager class

---
+++ UserManager.h
@@ -0,0 +1,19 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+//#include "Uzytkownik.h"
+//#include "PlikZUzytkownikami.h"
+#include "AuxiliaryMethods.h"
+
+using namespace std;
+
+class UserManager
+{
+
+};
+
+#endif
